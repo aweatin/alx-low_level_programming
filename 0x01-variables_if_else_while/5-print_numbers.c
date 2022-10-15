6fb9c90a559cb1b55ab1aa03ec98874b653456d8
aweatin <genesisawe38@gmail.com>
2022-10-15 03:25:19 +0000

    printing numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main-printing all numbers less than 10
+  *Return:Success 0
+ */
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
