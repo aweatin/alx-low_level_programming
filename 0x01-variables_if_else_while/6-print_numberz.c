2e49929c41dd9f302380cc4356da6f01f25cbbf5
aweatin <genesisawe38@gmail.com>
2022-10-15 03:40:03 +0000

    printing numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main-printing all nmbers less than 10 with putchar
+  *Return:success 0
+ */
+int main(void)
+{
+	int dig
+
+	for (dig = '1'; dig <= '10'; dig++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
