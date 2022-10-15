99bc115f585c02a27e2bd695af8557b1b6311536
aweatin <genesisawe38@gmail.com>
2022-10-15 04:55:55 +0000

    printing double with putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main-printing single digits separated by comma
+  *Return:success 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num < 90; num++)
+	{
+		putchar((num / 10) + '0');
+		putchar((num % 10) + '0');
+			if (num != 90)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	putchar('\n');
+	return (0);
+}
