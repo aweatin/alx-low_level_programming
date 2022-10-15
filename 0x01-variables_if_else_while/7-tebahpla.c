9cc6cda5af7fa0ef1d2ee5b54894cded633f76f2
aweatin <genesisawe38@gmail.com>
2022-10-15 03:49:03 +0000

    printing letters in reverse

---
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main-printing all small alphabets in reverse
+  *Return:success 0
+ */
+int main(void)
+{
+	char pla;
+
+	for (pla = 'z'; pla <= 'a'; pla++)
+	{
+		putchar(pla);
+	}
+	putchar('\n');
+	return (0);
+}
