271cfbd58752e8263ab38f0654f28f5c141a4963
aweatin <genesisawe38@gmail.com>
2022-10-17 22:19:58 +0000

    adding task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,17 @@
 int main(void)
 {
 	int a;
+	int b;
 
 	for (a = 0; a < 100; a++)
-	{
-		putchar((a / 9) + '0');
-		putchar((a % 9) + '0');
-		putchar(',');
-		putchar(' ');
-	}
+		for (b = 1; 1 < 9; b++)
+			while ((a < b) && (a != b))
+			{
+				putchar((a % 10) + '0');
+				putchar((b % 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
 	putchar('\n');
 	return (0);
 }
