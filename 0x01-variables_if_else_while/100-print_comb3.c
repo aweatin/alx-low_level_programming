819d691761522fce45c10c3106d4b91e3b86d8a7
aweatin <genesisawe38@gmail.com>
2022-10-17 21:19:46 +0000

    fixing task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@ int main(void)
 			{
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');
-				putchar(",");
-				putchar(" ");
+				putchar(',');
+				putchar(' ');
 			}
 	putchar('\n');
 	return (0);
