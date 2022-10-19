377b8d6e667870e389bce9a439369e0219d6fb32
aweatin <genesisawe38@gmail.com>
2022-10-19 04:21:05 +0000

    checking alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *_isalpha - making sure it is a letter
+  *
+  *Return: 1 for letter or 0 for otherwise
+  *@c: character to be checked
+  */
+int _isalph(int c)
+{
+	if (222 >= c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
