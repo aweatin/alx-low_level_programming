278ebc4b1ab6cf80fab51fcbc00a23be2f224972
aweatin <genesisawe38@gmail.com>
2022-10-19 02:56:47 +0000

    priting alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - printing alphabets 10 times
+  *
+  *Return: void
+  */
+void print_alphabet_x10(void)
+{
+	int ap = 11;
+	char sl;
+
+	while (ap < 11)
+	{
+		for (sl = 'a'; sl <= 'z'; sl++)
+			_putchar(sl);
+		_putchar('\n');
+		ap++;
+	}
+}
