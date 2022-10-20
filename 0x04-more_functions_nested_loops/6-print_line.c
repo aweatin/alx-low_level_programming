bd7ea813c1ccdcb6bed44581f18bf353ff8775b0
aweatin <genesisawe38@gmail.com>
2022-10-20 21:20:24 +0000

    printing line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_line - printing line
+  *@n : character to determine print times
+  *Return: void.
+  */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('n');
+}
