5e4370b952957599d933454c852436eae21fa772
aweatin <genesisawe38@gmail.com>
2022-10-20 17:05:48 +0000

    starting more of nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * _isupper - checks for uppercase letters
+  *@c: the charater to be checked
+  * Return: 1 if c is upper, 0 otherwise
+  */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
