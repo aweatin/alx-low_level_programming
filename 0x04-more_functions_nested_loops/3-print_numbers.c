1a83e710ddb6ce9b15e8111e247d03e19eb822d8
aweatin <genesisawe38@gmail.com>
2022-10-20 18:44:51 +0000

    printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_numbers - printing of numbers less than 10
+  *
+  *Return: print numbers
+  */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
