7321a473348f5da4f52b113440bcbe129bbc2aa2
aweatin <genesisawe38@gmail.com>
2022-10-20 17:16:38 +0000

    printing only digit to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * _isdigit - checks for uppercase letters
+  *@c: the charater to be checked
+  * Return: 1 if c is upper, 0 otherwise
+  */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 0);
+}
