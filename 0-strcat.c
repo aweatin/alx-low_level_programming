3f942dd25c2dd889603961199b174c2832732f9b
aweatin <genesisawe38@gmail.com>
2022-10-27 08:55:03 +0000

    con cating strings

---
+++ 0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _strncat - cats data
+  * @dest: where to copy to
+  * @src: bytess to be overwritten
+  * @n: integer variable
+  *Return: empty
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = src;
+	return (dest);
+}
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+char *_strcat(char *dest, char *src);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
