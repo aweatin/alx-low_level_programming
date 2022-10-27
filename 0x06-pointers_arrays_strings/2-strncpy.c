fcd91049eef1231ead1d7546fba7a774c2ee3d5f
aweatin <genesisawe38@gmail.com>
2022-10-27 19:06:36 +0000

    copying strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_strncpy - copying two strings
+*@dest : pointer
+*@src : source pointer
+*@n : integer
+*Return: Always 0.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
+
+	for (; j < a && src[b] != '\0'; j++)
+	{
+		dest[j] = src[b];
+	}
+	for (; j < n; j++)
+		dest[j] = '\0';
+	return (dest);
+}
