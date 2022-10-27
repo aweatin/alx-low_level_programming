a0efdcc4ac76e24025e6c57ac7b2d7b93a9f459c
aweatin <113603105+aweatin@users.noreply.github.com>
2022-10-27 17:31:20 +0000

    re-edinting approach

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
-  * _strncat - continates two strings
-  * @dest: the string to append
-  * @src: the string to append dest
-  * @n: the number of bytes from src
-  *
-  *Return: a ponter to the resulting string dest
-  */
-char *_strncat(char *dest, char src, int n)
+ * _strncat - function to concatenate strings
+ * @dest : pointer
+ * @src : pointer
+ * @n : variable integer
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int a, b;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
 	return (dest);
 }
