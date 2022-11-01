4e134da705a7a3e449f064647190cdb0dd6bc002
aweatin <genesisawe38@gmail.com>
2022-11-01 16:04:13 +0000

    printing chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * print_chessboard - print chessboard
+  * @a: pointer
+  * Return: Always 0
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = o; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
