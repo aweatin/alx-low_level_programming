229a182daa562bffdf69ad50a85a0e0d938900df
aweatin <genesisawe38@gmail.com>
2022-11-11 14:45:53 +0000

    doing more malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "mian.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _calloc - function that allocates memory for an array
+  * @nmemb: number
+  * @size: size of
+  * Return: NULL if failed.
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int n;
+	void *hol;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+		hol = p;
+	if (p == 0)
+		return (NULL);
+	for (n = 0; n < (nmemb * size); n++)
+		p[n] = 0;
+	return (hol);
+}
